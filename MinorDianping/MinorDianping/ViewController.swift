0606b0b5b72810dd11f9b8b6dd18421dfcf61972
Zhijian Jiang <Zhijian.Jiang@outlook.com>
2017-05-22 19:53:12 +0800

    finish core data and parse csv files

--- MinorDianping/MinorDianping/ViewController.swift
+++ MinorDianping/MinorDianping/ViewController.swift
@@ -10,26 +10,78 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
+    let FIRST_RUN = false
+    
+    // MARK: CSV File Functions
+    private func cleanRows(file:String) -> String{
+        // use a uniform \n for end of lines
+        var cleanFile = file
+        cleanFile = cleanFile.replacingOccurrences(of: "\r", with: "\n")
+        cleanFile = cleanFile.replacingOccurrences(of: "\n\n", with: "\n")
+        return cleanFile
+    }
+    
+    private func getStringFieldsForRow(row: String, delimiter:String) -> [String]{
+        return row.components(separatedBy: delimiter)
+    }
+    
+    func convertCSV(file:String) -> [[String:String]]?{
+        let rows = cleanRows(file: file).components(separatedBy: "\n")
+        if rows.count > 0 {
+            var data = [[String:String]]()
+            let columnTitles = getStringFieldsForRow(row: rows.first!, delimiter: ",")
+            for row in rows{
+                let fields = getStringFieldsForRow(row: row, delimiter: ",")
+                if fields.count != columnTitles.count{continue}
+                var dataRow = [String:String]()
+                for (index, field) in fields.enumerated(){
+                    dataRow[columnTitles[index]] = field
+                }
+                data += [dataRow]
+            }
+            return data
+        }
+        else {
+            print("No data in file")
+        }
+        return nil
+    }
+    
+    // MARK: Data reading and writing functions
+    func readDataFromFile(file:String, type: String) -> String!{
+        guard let filepath = Bundle.main.path(forResource: file, ofType: type)
+            else{
+                return nil
+        }
+        do {
+            let contents = try String(contentsOfFile: filepath, encoding: String.Encoding.utf8)
+            return contents
+        } catch {
+            print("File Read Error")
+            return nil
+        }
+    }
+    
+//    func fetchOneResultFromCoreData<T:NSManagedObject>(index: Int) -> T?{
+//        let fetchRequest:NSFetchRequest<T> = T.fetchRequest() as! NSFetchRequest<T>
+//        do{
+//            let searchResults = try DatabaseController.getContext().fetch(fetchRequest)
+//            return searchResults[index]
+//        }
+//        catch{
+//            print("Error: \(error)")
+//
+//        }
+//        return nil
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // MARK: - Core Data operation
-        let cityClassName:String = String(describing: City.self)
-        let restaurantClassName:String = String(describing: Restaurant.self)
         
-        let restaurant:Restaurant = NSEntityDescription.insertNewObject(forEntityName: restaurantClassName, into: DatabaseController.getContext()) as! Restaurant
-        restaurant.location = "Xianlin"
-        restaurant.name = "KFC"
-        
-        let city:City = NSEntityDescription.insertNewObject(forEntityName: cityClassName, into: DatabaseController.getContext()) as! City
-        city.cityName = "Nanjing"
-        city.addToRestaurants(restaurant)
-        
+        // MARK: Core Data Fetch Function
 
-        DatabaseController.saveContext()
-        
         let fetchRequest:NSFetchRequest<Restaurant> = Restaurant.fetchRequest()
         
         do{
@@ -44,9 +96,36 @@ class ViewController: UIViewController {
         catch{
             print("Error: \(error)")
         }
+//        let restaurant:Restaurant = fetchOneResultFromCoreData(index: 0)! as! Restaurant
+//        print(restaurant.location)
         
-        DatabaseController.getContext().delete(city)
-        DatabaseController.getContext().delete(restaurant)
+        if(FIRST_RUN){
+            // MARK: CSV File Operations
+            let file:String = readDataFromFile(file: "Nanjing", type: "csv")
+            guard let contents:[[String:String]] = convertCSV(file: file)else {
+                print("Parse Successfully")
+                return
+            }
+            
+            let numOfRestaurants = contents.count - 1
+            
+            // MARK: - Core Data Operations
+            let cityClassName:String = String(describing: City.self)
+            let restaurantClassName:String = String(describing: Restaurant.self)
+            
+            let city:City = NSEntityDescription.insertNewObject(forEntityName: cityClassName, into: DatabaseController.getContext()) as! City
+            city.cityName = "Nanjing"
+            
+            for i in 0 ..< numOfRestaurants{
+                let restaurant:Restaurant = NSEntityDescription.insertNewObject(forEntityName: restaurantClassName, into: DatabaseController.getContext()) as! Restaurant
+                restaurant.location = contents[i + 1]["位置"]
+                restaurant.name = contents[i + 1]["餐厅名"]
+                city.addToRestaurants(restaurant)
+            }
+            
+            DatabaseController.saveContext()
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # MinorDianping
 This the final project of NJU Mobile Application 2017 Spring by Zhijian Jiang and Zhecheng Li.   
+
+## 开发过程中的Swift笔记
+### Bundle
+* Swift中用于读取工程目录信息的类。
+### 一些关键词
+* guard：可以暂时理解为类似于if关键词，必须有else。
+
+### 泛型
+#### 类型约束
+```
+func someFunction<T: SomeClass, U: SomeProtocol>(someT: T, someU: U) {
+    // function body goes here
+}
+```
+
+### 常用技术
+#### 从文件中读取内容
+1. 将想要读取的文件添加到xcode的工程目录中，也就是添加到Bundle中。
+2. 编写代码，以csv文件为例：
+```
+    guard let filepath = Bundle.main.path(forResource: file, ofType: "csv")
+        else{
+            return nil
+    }
+    do {
+        let contents = try String(contentsOfFile: filepath, encoding: String.Encoding.utf8)
+        return contents
+    } catch {
+        print("File Read Error")
+        return nil
+    }
+```
+
+#### 错误处理
+
+#### if let和guard语法糖
+```
+if let v = str{
+// use v to do something
+}
+
+guard v = str else {return}
+// use v to do something
+```
+
+#### for循环
+```
+for i in 0 ... 5
+```
+从0开始包括5；
+```
+for i in 0 ..< 5
+```
+从0开始不包括5。
+
+### 各种报错信息
+* [Cannot explicitly specialize a generic function](http://stackoverflow.com/questions/27965439/cannot-explicitly-specialize-a-generic-function)
+
+## 其他笔记
+### Git
+* git push [remote-name] [branch-name]
+* [5.2 代码回滚：Reset、Checkout、Revert的选择](https://github.com/geeeeeeeeek/git-recipes/wiki/5.2-%E4%BB%A3%E7%A0%81%E5%9B%9E%E6%BB%9A%EF%BC%9AReset%E3%80%81Checkout%E3%80%81Revert%E7%9A%84%E9%80%89%E6%8B%A9)
+    * 撤销commit：git reset改变commit历史，git revert不改变commit历史；所以git reset适用于私有，revert适用于公有。
+    * 显示当前分支：git branch
+### 常用命令
+* 查看文件的编码方式：file命令。
