faaee423aa35d81ddae1b54fdebd5717e0bec9fa
Zhijian Jiang <Zhijian.Jiang@outlook.com>
2017-06-01 19:33:22 +0800

    写好了一部分服务器端代码但是有bug

---
+++ php/register.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2017/5/31
+ * Time: 下午10:42
+ */
+
+// STEP 1. Declare params of user info
+// Secruing info and storing var
+$username = htmlentities($_REQUEST["username"]);
+$password = htmlentities($_REQUEST["password"]);
+$email = htmlentities($_REQUEST["email"]);
+$fullname = htmlentities($_REQUEST["fullname"]);
+
+
+// if GET or POST are empty
+if (empty($username) || empty($password) || empty($email) || empty($fullname)){
+
+    $returnArray["status"] = "400";
+    $returnArray["message"] = "Missing required information";
+    echo json_encode($returnArray);
+    return;
+}
+
+// secure password
+$salt = openssl_random_pseudo_bytes(20);
+$secured_password = sha1($password . $salt);
+
+// STEP 2. Build connection
+// Secure way to build conn
+$file = parse_ini_file("../../../minor_dianping.ini");
+
+// store in php var inf from ini var
+$host = trim($file["dbhost"]);
+$user = trim($file["dbuser"]);
+$pass = trim($file["dbpass"]);
+$name = trim($file["dbname"]);
+
+// include access.php to call func from access.php file
+require("secure/access.php");
+$access = new access($host, $user, $pass, $name);
+$access->connect();
+
+// STEP 3. Register user information
+$result = $access->registerUser($username, $secured_password, $salt, $email, $fullname);
+
+if(!$result){
+    $user = $access->selectUser($username);
+    $returnArray["status"] = "200";
+    $returnArray["message"] = "Successfully registered";
+    $returnArray["id"] = $user["id"];
+    $returnArray["username"] = $user["username"];
+    $returnArray["fullname"] = $user["fullname"];
+    $returnArray["ava"] = $user["ava"];
+
+
+}else{
+    $returnArray["status"] = "400";
+    $returnArray["message"] = "Could not register with provided information";
+}
+
+// STEP 4. Close Connection
+$access->disconnect();
+
+//STEP 5. Json data
+echo json_encode($returnArray);
+
+?>
---
+++ php/secure/access.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2017/5/31
+ * Time: 下午10:18
+ */
+// Declare class to access to this php file
+class access {
+
+    // connection global variables
+    var $host = null;
+    var $user = null;
+    var $pass = null;
+    var $name = null;
+    var $conn = null;
+    var $result = null;
+
+    // constructing class
+    function __construct($dbhost, $dbuser, $dbpass, $dbname){
+
+        $this->host = $dbhost;
+        $this->user = $dbuser;
+        $this->pass = $dbpass;
+        $this->name = $dbname;
+
+    }
+
+    // connection function
+    public function connect(){
+
+        // establish connection and store it in $conn
+        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->name);
+
+        if(mysqli_connect_errno()) {
+            echo "Could not connect to database";
+        }
+
+        // support all languages
+        $this->conn->set_charset("utf8");
+
+    }
+
+    // disconnection function
+    public function disconnect(){
+
+        if($this->conn != null){
+            $this->conn->close();
+        }
+
+    }
+
+    // Insert user
+    public function registerUser($username, $password, $salt, $email, $fullname){
+        // sql command
+        $sql = "INSERT INTO users SET username=?, password=?, salt=?, email=?, fullname=?";
+
+        // store query result in $statement
+        $statement = $this->conn->prepare($sql);
+
+        // if error
+        if(!$statement){
+            throw new Exception($statement->error);
+        }
+
+        // bind 5 param of type string to be placed in $sql command
+        $statement->bind_param("sssss", $username, $password, $salt, $email, $fullname);
+
+        $returnValue = $statement->execute();
+        return $returnValue;
+    }
+
+    public function selectUser($username){
+        $returnArray = null;
+        // sql command
+        $sql = "SELECT * FROM users WHERE username='".$username."'";
+
+        // assign result we got from $sql to $result var
+        $result = $this->conn->query($sql);
+        if($result != null && (mysqli_num_rows($result) >= 1)){
+            echo "into";
+            // assign results we got to $row as associative array
+            $row = $result->fetch_array(MYSQLI_ASSOC);
+            echo "row" . $row["id"];
+            if(!empty($row)){
+                $returnArray = $row;
+            }
+        }
+        return $returnArray;
+    }
+}
+
+?>
